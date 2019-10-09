acee8754933361bf78da20509c1adfcd666d540a
IamMaxim <g.maxim.stepanoff@gmail.com>
2019-10-09 14:59:16 +0300

    Added comment for the 2nd exercise

--- week08/ex2.c
+++ week08/ex2.c
@@ -2,6 +2,11 @@
 #include <string.h>
 #include <unistd.h>
 
+/*
+    vmstat/vm_stat prints 0 in is and so (swapins/swapouts), because there
+    is plenty of physical memory available and swapping is not needed.
+*/
+
 int main() {
     for (int i = 0; i < 10; i++) {
         void *newMem = malloc(10 * 1024 * 1024);
